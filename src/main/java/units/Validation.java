0f824127b5bfb317fdab2d7d293c384b2b698fdd
Jeff Langr <jeff@langrsoft.com>
2024-06-27 14:48:31 -0600

    test for validation

--- src/main/java/units/Validation.java
+++ src/main/java/units/Validation.java
@@ -3,7 +3,6 @@ package units;
 // START:Validation
 interface Validation {
     boolean isInvalid();
-
     String errorMessage();
 }
 // START:Validation
---
+++ src/test/java/units/AValidator.java
@@ -0,0 +1,35 @@
+package units;
+
+// START:impl
+import org.junit.jupiter.api.Test;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AValidator {
+   Validation passingValidation = new Validation() {
+      @Override public boolean isInvalid() { return false; }
+      @Override public String errorMessage() { return ""; }
+   };
+
+   Validation failingValidation = new Validation() {
+      @Override public boolean isInvalid() { return true; }
+      @Override public String errorMessage() { return "fail"; }
+   };
+
+   @Test
+   void returnsEmptyListWhenAllValidationsPass() {
+      assertEquals(Collections.emptyList(),
+         new Validator().validate(List.of(passingValidation)));
+   }
+
+   @Test
+   void returnsListOfFailingValidationMessages() {
+      assertEquals(List.of(failingValidation.errorMessage()),
+         new Validator().validate(List.of(
+            failingValidation,
+            passingValidation)));
+   }
+}
+// END:impl
