f0049c12f40367ee023899e7e6091fc13b9af446
Jeff Langr <jeff@langrsoft.com>
2024-06-27 13:46:49 -0600

    better names ABooking

--- src/test/java/units/ABooking.java
+++ src/test/java/units/ABooking.java
@@ -22,7 +22,7 @@ class ABooking {
    static final String VALID_NAME = "Aslan";
 
    @Nested
-   class validateReturnsNoErrorMessageWhen {
+   class ValidatesWhen {
       @Test
       void allFieldsAreValid() {
          var booking = new Booking(VALID_NAME, VALID_AGE, VALID_DEPARTURE_DATE, VALID_ITINERARY);
@@ -34,7 +34,7 @@ class ABooking {
    }
 
    @Nested
-   class validateReturnsErrorMessageWhen {
+   class DoesNotValidateWhen {
       @Test
       void nameIsNull() {
          String name = null;
